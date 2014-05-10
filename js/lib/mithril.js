4280c7b7e193048d836acb8eb2ef38bbf85d5602
Eirik S. Morland <eirik@morland.no>
2014-05-10 16:28:04 +0200

    Update mithril to 0.1.12

--- js/lib/mithril.js
+++ js/lib/mithril.js
@@ -1,9 +1,9 @@
-new function(window) {
+Mithril = m = new function app(window) {
 	var selectorCache = {}
 	var type = {}.toString
 	var parser = /(?:(^|#|\.)([^#\.\[\]]+))|(\[.+?\])/g, attrParser = /\[(.+?)(?:=("|'|)(.+?)\2)?\]/
 	
-	Mithril = m = function() {
+	function m() {
 		var args = arguments
 		var hasAttrs = type.call(args[1]) == "[object Object]"
 		var attrs = hasAttrs ? args[1] : {}
@@ -32,8 +32,9 @@ new function(window) {
 		}
 		return cell
 	}
-	function build(parent, data, cached) {
-		if (data === null || data === undefined) return
+	function build(parentElement, parentTag, data, cached, shouldReattach, index, editable, namespace) {
+		if (data === null || data === undefined) data = ""
+		if (data.subtree === "retain") return
 		
 		var cachedType = type.call(cached), dataType = type.call(data)
 		if (cachedType != dataType) {
@@ -43,36 +44,44 @@ new function(window) {
 		}
 		
 		if (dataType == "[object Array]") {
-			var nodes = [], intact = cached.length === data.length
-			for (var i = 0; i < data.length; i++) {
-				var item = build(parent, data[i], cached[i])
+			var nodes = [], intact = cached.length === data.length, subArrayCount = 0
+			for (var i = 0, cacheCount = 0; i < data.length; i++) {
+				var item = build(parentElement, null, data[i], cached[cacheCount], shouldReattach, index + subArrayCount || subArrayCount, editable, namespace)
 				if (item === undefined) continue
 				if (!item.nodes.intact) intact = false
-				cached[i] = item
+				subArrayCount += item instanceof Array ? item.length : 1
+				cached[cacheCount++] = item
 			}
 			if (!intact) {
 				for (var i = 0; i < data.length; i++) if (cached[i] !== undefined) nodes = nodes.concat(cached[i].nodes)
 				for (var i = nodes.length, node; node = cached.nodes[i]; i++) if (node.parentNode !== null) node.parentNode.removeChild(node)
-				for (var i = cached.nodes.length, node; node = nodes[i]; i++) if (node.parentNode === null) parent.appendChild(node)
-				cached.length = data.length
+				for (var i = cached.nodes.length, node; node = nodes[i]; i++) if (node.parentNode === null) parentElement.appendChild(node)
+				if (data.length < cached.length) cached.length = data.length
 				cached.nodes = nodes
 			}
 		}
 		else if (dataType == "[object Object]") {
+			if (data.tag != cached.tag || Object.keys(data.attrs).join() != Object.keys(cached.attrs).join() || data.attrs.id != cached.attrs.id) clear(cached.nodes)
 			if (typeof data.tag != "string") return
-			if (data.tag != cached.tag || Object.keys(data.attrs).join() != Object.keys(cached.attrs).join()) clear(cached.nodes)
 			
 			var node, isNew = cached.nodes.length === 0
+			if (data.tag === "svg") namespace = "http://www.w3.org/2000/svg"
 			if (isNew) {
-				node = window.document.createElement(data.tag)
-				cached = {tag: data.tag, attrs: setAttributes(node, data.attrs, {}), children: build(node, data.children, cached.children), nodes: [node]}
-				parent.appendChild(node)
+				node = namespace === undefined ? window.document.createElement(data.tag) : window.document.createElementNS(namespace, data.tag)
+				cached = {
+					tag: data.tag,
+					attrs: setAttributes(node, data.tag, data.attrs, {}, namespace),
+					children: build(node, data.tag, data.children, cached.children, true, 0, data.attrs.contenteditable ? node : editable, namespace),
+					nodes: [node]
+				}
+				parentElement.insertBefore(node, parentElement.childNodes[index] || null)
 			}
 			else {
 				node = cached.nodes[0]
-				setAttributes(node, data.attrs, cached.attrs)
-				cached.children = build(node, data.children, cached.children)
+				setAttributes(node, data.tag, data.attrs, cached.attrs, namespace)
+				cached.children = build(node, data.tag, data.children, cached.children, false, 0, data.attrs.contenteditable ? node : editable, namespace)
 				cached.nodes.intact = true
+				if (shouldReattach === true) parentElement.insertBefore(node, parentElement.childNodes[index] || null)
 			}
 			if (type.call(data.attrs["config"]) == "[object Function]") data.attrs["config"](node, !isNew)
 		}
@@ -80,33 +89,35 @@ new function(window) {
 			var node
 			if (cached.nodes.length === 0) {
 				if (data.$trusted) {
-					var lastChild = parent.lastChild
-					parent.insertAdjacentHTML("beforeend", data)
-					node = lastChild ? lastChild.nextSibling : parent.firstChild
+					node = injectHTML(parentElement, index, data)
 				}
 				else {
 					node = window.document.createTextNode(data)
-					parent.appendChild(node)
+					parentElement.insertBefore(node, parentElement.childNodes[index] || null)
 				}
 				cached = "string number boolean".indexOf(typeof data) > -1 ? new data.constructor(data) : data
 				cached.nodes = [node]
 			}
-			else if (cached.valueOf() !== data.valueOf()) {
-				if (data.$trusted) {
-					var current = cached.nodes[0], nodes = [current]
-					if (current) {
-						while (current = current.nextSibling) nodes.push(current)
-						clear(nodes)
-						var lastChild = parent.lastChild
-						parent.insertAdjacentHTML("beforeend", data)
-						node = lastChild ? lastChild.nextSibling : parent.firstChild
+			else if (cached.valueOf() !== data.valueOf() || shouldReattach === true) {
+				if (!editable || editable !== window.document.activeElement) {
+					if (data.$trusted) {
+						var current = cached.nodes[0], nodes = [current]
+						if (current) {
+							while (current = current.nextSibling) nodes.push(current)
+							clear(nodes)
+							node = injectHTML(parentElement, index, data)
+						}
+						else parentElement.innerHTML = data
 					}
-					else parent.innerHTML = data
-				}
-				else {
-					node = cached.nodes[0]
-					parent.appendChild(node)
-					node.nodeValue = data
+					else {
+						node = cached.nodes[0]
+						if (parentTag === "textarea") parentElement.value = data
+						else if (editable) editable.innerHTML = data
+						else {
+							parentElement.insertBefore(node, parentElement.childNodes[index] || null)
+							node.nodeValue = data
+						}
+					}
 				}
 				cached = new data.constructor(data)
 				cached.nodes = [node]
@@ -116,23 +127,44 @@ new function(window) {
 		
 		return cached
 	}
-	function setAttributes(node, dataAttrs, cachedAttrs) {
+	function setAttributes(node, tag, dataAttrs, cachedAttrs, namespace) {
 		for (var attrName in dataAttrs) {
 			var dataAttr = dataAttrs[attrName]
-			if (!(attrName in cachedAttrs) || (cachedAttrs[attrName] !== dataAttr)) {
+			var cachedAttr = cachedAttrs[attrName]
+			if (!(attrName in cachedAttrs) || (cachedAttr !== dataAttr) || node === window.document.activeElement) {
 				cachedAttrs[attrName] = dataAttr
-				if (attrName == "config") continue
-				if (attrName.indexOf("on") == 0 && typeof dataAttr == "function") dataAttr = autoredraw(dataAttr, node)
-				if (attrName == "style") for (var rule in dataAttr) node.style[rule] = dataAttr[rule]
-				else if (attrName in node) node[attrName] = dataAttr
+				if (attrName === "config") continue
+				else if (typeof dataAttr == "function" && attrName.indexOf("on") == 0) {
+					node[attrName] = autoredraw(dataAttr, node)
+				}
+				else if (attrName === "style") {
+					for (var rule in dataAttr) {
+						if (cachedAttr === undefined || cachedAttr[rule] !== dataAttr[rule]) node.style[rule] = dataAttr[rule]
+					}
+				}
+				else if (namespace !== undefined) {
+					if (attrName === "href") node.setAttributeNS("http://www.w3.org/1999/xlink", "href", dataAttr)
+					else if (attrName === "className") node.setAttribute("class", dataAttr)
+					else node.setAttribute(attrName, dataAttr)
+				}
+				else if (attrName === "value" && tag === "input") {
+					if (node.value !== dataAttr) node.value = dataAttr
+				}
+				else if (attrName in node && attrName != "list") node[attrName] = dataAttr
 				else node.setAttribute(attrName, dataAttr)
 			}
 		}
 		return cachedAttrs
 	}
 	function clear(nodes) {
-		for (var i = 0; i < nodes.length; i++) nodes[i].parentNode.removeChild(nodes[i])
+		for (var i = nodes.length - 1; i > -1; i--) nodes[i].parentNode.removeChild(nodes[i])
 		nodes.length = 0
+	}
+	function injectHTML(parentElement, index, data) {
+		var nextSibling = parentElement.childNodes[index]
+		if (nextSibling) nextSibling.insertAdjacentHTML("beforebegin", data)
+		else parentElement.insertAdjacentHTML("beforeend", data)
+		return nextSibling ? nextSibling.previousSibling : parentElement.firstChild
 	}
 	function clone(object) {
 		var result = {}
@@ -140,11 +172,10 @@ new function(window) {
 		return result
 	}
 	function autoredraw(callback, object) {
-		return function() {
+		return function(e) {
 			m.startComputation()
-			var output = callback.apply(object || window, arguments)
-			m.endComputation()
-			return output
+			try {return callback.call(object, e)}
+			finally {m.endComputation()}
 		}
 	}
 	
@@ -161,14 +192,18 @@ new function(window) {
 			if (window.document.documentElement !== html) {
 				window.document.replaceChild(html, window.document.documentElement)
 			}
-		}
+		},
+		insertBefore: function(node) {
+			this.appendChild(node)
+		},
+		childNodes: []
 	}
 	var nodeCache = [], cellCache = {}
 	m.render = function(root, cell) {
 		var index = nodeCache.indexOf(root)
 		var id = index < 0 ? nodeCache.push(root) - 1 : index
 		var node = root == window.document || root == window.document.documentElement ? documentNode : root
-		cellCache[id] = build(node, cell, cellCache[id])
+		cellCache[id] = build(node, null, cell, cellCache[id], false, 0, null, undefined)
 	}
 	
 	m.trust = function(value) {
@@ -177,40 +212,42 @@ new function(window) {
 		return value
 	}
 	
-	var currentRoot, currentModule = {view: function() {}}, currentController = {}, now = 0, lastRedraw = 0, lastRedrawId = 0
+	var roots = [], modules = [], controllers = [], now = 0, lastRedraw = 0, lastRedrawId = 0, computePostRedrawHook = null
 	m.module = function(root, module) {
 		m.startComputation()
-		currentRoot = root
-		currentModule = module
-		currentController = new module.controller
+		var index = roots.indexOf(root)
+		if (index < 0) index = roots.length
+		roots[index] = root
+		modules[index] = module
+		controllers[index] = new module.controller
 		m.endComputation()
 	}
 	m.redraw = function() {
-		m.render(currentRoot || window.document, currentModule.view(currentController))
-		lastRedraw = now
-	}
-	function redraw() {
 		now = window.performance && window.performance.now ? window.performance.now() : new window.Date().getTime()
-		if (now - lastRedraw > 16) m.redraw()
+		if (now - lastRedraw > 16) redraw()
 		else {
 			var cancel = window.cancelAnimationFrame || window.clearTimeout
 			var defer = window.requestAnimationFrame || window.setTimeout
 			cancel(lastRedrawId)
-			lastRedrawId = defer(m.redraw, 0)
+			lastRedrawId = defer(redraw, 0)
 		}
 	}
+	function redraw() {
+		for (var i = 0; i < roots.length; i++) {
+			m.render(roots[i], modules[i].view(controllers[i]))
+		}
+		if (computePostRedrawHook) {
+			computePostRedrawHook()
+			computePostRedrawHook = null
+		}
+		lastRedraw = now
+	}
 	
-	var pendingRequests = 0, computePostRedrawHook = null
+	var pendingRequests = 0
 	m.startComputation = function() {pendingRequests++}
 	m.endComputation = function() {
 		pendingRequests = Math.max(pendingRequests - 1, 0)
-		if (pendingRequests == 0) {
-			redraw()
-			if (computePostRedrawHook) {
-				computePostRedrawHook()
-				computePostRedrawHook = null
-			}
-		}
+		if (pendingRequests == 0) m.redraw()
 	}
 	
 	m.withAttr = function(prop, withAttrCallback) {
@@ -219,9 +256,11 @@ new function(window) {
 	
 	//routing
 	var modes = {pathname: "", hash: "#", search: "?"}
-	var redirect = function() {}, routeParams = {}
+	var redirect = function() {}, routeParams = {}, currentRoute
 	m.route = function() {
-		if (arguments.length == 3) {
+		if (arguments.length === 0) return currentRoute
+		else if (arguments.length === 3) {
+			currentRoute = window.location[m.route.mode].slice(modes[m.route.mode].length)
 			var root = arguments[0], defaultRoute = arguments[1], router = arguments[2]
 			redirect = function(source) {
 				var path = source.slice(modes[m.route.mode].length)
@@ -239,22 +278,25 @@ new function(window) {
 		else if (arguments[0].addEventListener) {
 			var element = arguments[0]
 			var isInitialized = arguments[1]
+			if (element.href.indexOf(modes[m.route.mode]) < 0) {
+				element.href = location.pathname + modes[m.route.mode] + element.pathname
+			}
 			if (!isInitialized) {
 				element.removeEventListener("click", routeUnobtrusive)
 				element.addEventListener("click", routeUnobtrusive)
 			}
 		}
 		else if (typeof arguments[0] == "string") {
-			var route = arguments[0]
+			currentRoute = arguments[0]
 			var shouldReplaceHistoryEntry = arguments[1] === true
 			if (window.history.pushState) {
 				computePostRedrawHook = function() {
-					window.history[shouldReplaceHistoryEntry ? "replaceState" : "pushState"](null, window.document.title, modes[m.route.mode] + route)
+					window.history[shouldReplaceHistoryEntry ? "replaceState" : "pushState"](null, window.document.title, modes[m.route.mode] + currentRoute)
 					scrollToHash()
 				}
-				redirect(modes[m.route.mode] + route)
+				redirect(modes[m.route.mode] + currentRoute)
 			}
-			else window.location[m.route.mode] = route
+			else window.location[m.route.mode] = currentRoute
 		}
 	}
 	m.route.param = function(key) {return routeParams[key]}
@@ -264,20 +306,22 @@ new function(window) {
 		for (var route in router) {
 			if (route == path) return !void m.module(root, router[route])
 			
-			var matcher = new RegExp("^" + route.replace(/:[^\/]+/g, "([^\\/]+)") + "$")
+			var matcher = new RegExp("^" + route.replace(/:[^\/]+?\.{3}/g, "(.*?)").replace(/:[^\/]+/g, "([^\\/]+)") + "$")
+			
 			if (matcher.test(path)) {
 				return !void path.replace(matcher, function() {
 					var keys = route.match(/:[^\/]+/g)
 					var values = [].slice.call(arguments, 1, -2)
-					for (var i = 0; i < keys.length; i++) routeParams[keys[i].slice(1)] = values[i]
+					for (var i = 0; i < keys.length; i++) routeParams[keys[i].replace(/:|\./g, "")] = decodeURIComponent(values[i])
 					m.module(root, router[route])
 				})
 			}
 		}
 	}
 	function routeUnobtrusive(e) {
+		if (e.ctrlKey || e.metaKey || e.which == 2) return
 		e.preventDefault()
-		m.route(e.currentTarget.getAttribute("href"))
+		m.route(e.currentTarget[m.route.mode].slice(modes[m.route.mode].length))
 	}
 	function scrollToHash() {
 		if (m.route.mode != "hash" && window.location.hash) window.location.hash = window.location.hash
@@ -285,10 +329,14 @@ new function(window) {
 	
 	//model
 	m.prop = function(store) {
-		return function() {
+		var prop = function() {
 			if (arguments.length) store = arguments[0]
 			return store
 		}
+		prop.toJSON = function() {
+			return store
+		}
+		return prop
 	}
 
 	m.deferred = function() {
@@ -307,14 +355,21 @@ new function(window) {
 			var next = m.deferred()
 			if (!success) success = identity
 			if (!error) error = identity
-			resolvers.push(function(value) {
-				var result = success(value)
-				next.resolve(result !== undefined ? result : value)
-			})
-			rejecters.push(function(value) {
-				var result = error(value)
-				next.reject(result !== undefined ? result : value)
-			})
+			function push(list, method, callback) {
+				list.push(function(value) {
+					try {
+						var result = callback(value)
+						if (result && typeof result.then == "function") result.then(next[method], error)
+						else next[method](result !== undefined ? result : value)
+					}
+					catch (e) {
+						if (e instanceof Error && e.constructor !== Error) throw e
+						else next.reject(e)
+					}
+				})
+			}
+			push(resolvers, "resolve", success)
+			push(rejecters, "reject", error)
 			return next.promise
 		}
 		return object
@@ -347,6 +402,11 @@ new function(window) {
 		xhr.open(options.method, options.url, true, options.user, options.password)
 		xhr.onload = typeof options.onload == "function" ? options.onload : function() {}
 		xhr.onerror = typeof options.onerror == "function" ? options.onerror : function() {}
+		xhr.onreadystatechange = function() {
+			if (xhr.readyState === 4 && xhr.status === 0) {
+				xhr.onerror({type: "error", target: xhr})
+			}
+		}
 		if (typeof options.config == "function") options.config(xhr, options)
 		xhr.send(options.data)
 		return xhr
@@ -369,7 +429,7 @@ new function(window) {
 		return xhrOptions
 	}
 	function parameterizeUrl(url, data) {
-		var tokens = url.match(/:\w+/g)
+		var tokens = url.match(/:[a-z]\w+/gi)
 		if (tokens && data) {
 			for (var i = 0; i < tokens.length; i++) {
 				var key = tokens[i].slice(1)
@@ -381,22 +441,25 @@ new function(window) {
 	}
 
 	m.request = function(xhrOptions) {
-		m.startComputation()
+		if (xhrOptions.background !== true) m.startComputation()
 		var deferred = m.deferred()
 		var serialize = xhrOptions.serialize || JSON.stringify
 		var deserialize = xhrOptions.deserialize || JSON.parse
+		var extract = xhrOptions.extract || function(xhr) {
+			return xhr.responseText.length === 0 && deserialize === JSON.parse ? null : xhr.responseText
+		}
 		xhrOptions.url = parameterizeUrl(xhrOptions.url, xhrOptions.data)
 		xhrOptions = bindData(xhrOptions, xhrOptions.data, serialize)
 		xhrOptions.onload = xhrOptions.onerror = function(e) {
 			var unwrap = (e.type == "load" ? xhrOptions.unwrapSuccess : xhrOptions.unwrapError) || identity
-			var response = unwrap(deserialize(e.target.responseText))
+			var response = unwrap(deserialize(extract(e.target, xhrOptions)))
 			if (response instanceof Array && xhrOptions.type) {
 				for (var i = 0; i < response.length; i++) response[i] = new xhrOptions.type(response[i])
 			}
 			else if (xhrOptions.type) response = new xhrOptions.type(response)
 			deferred.promise(response)
 			deferred[e.type == "load" ? "resolve" : "reject"](response)
-			m.endComputation()
+			if (xhrOptions.background !== true) m.endComputation()
 		}
 		ajax(xhrOptions)
 		deferred.promise.then = propBinder(deferred.promise)
@@ -413,4 +476,11 @@ new function(window) {
 	
 	//testing API
 	m.deps = function(mock) {return window = mock}
+	//for internal testing only, do not use `m.deps.factory`
+	m.deps.factory = app
+	
+	return m
 }(this)
+
+if (typeof module != "undefined" && module !== null) module.exports = m
+if (typeof define == "function" && define.amd) define(function() {return m})
