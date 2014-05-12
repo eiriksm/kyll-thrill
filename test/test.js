253008dda4e1fbedc867fd343e112f6753a6bff2
Eirik S. Morland <eirik@morland.no>
2014-05-12 08:46:23 +0200

    Try to route to start first.

--- test/test.js
+++ test/test.js
@@ -45,12 +45,13 @@
 
   describe('Disqus stuff', function () {
     it('Should add disqus widget if defined', function(done) {
+      m.route('/');
       disqus_shortname = 'kyllthrill';
       m.route('/blog/2014/03/21/power-of');
       // See if a script tag is added to head.
       setTimeout(function() {
-        // var h = document.getElementsByTagName('head')[0];
-        // assert.equal(h.childNodes[h.childNodes.length - 1].src.indexOf('kyllthrill.disqus.com') > 1, true);
+        var h = document.getElementsByTagName('head')[0];
+        assert.equal(h.childNodes[h.childNodes.length - 1].src.indexOf('kyllthrill.disqus.com') > 1, true);
         done();
       }, 100);
     });
